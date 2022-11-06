f4dbef57808cdc79bec79abd2bd59812d430be7e
hmtmcse <hmtm.cse@gmail.com>
2022-11-06 17:42:02 +0600

    added variant

--- boot/spec/ButtonSpec.tsx
+++ boot/spec/ButtonSpec.tsx
@@ -3,7 +3,7 @@ import {Size} from "./common/common-type";
 import PFBSReactComponent, {PFUIState} from "./common/spec-common-things";
 
 export type ButtonType = 'button' | 'reset' | 'submit'
-export type Variant = 'primary' | 'secondary' | 'danger'  | 'warning' | 'info' | 'light' | 'dark' | 'link'
+export type Variant = 'primary' | 'secondary' | 'danger'  | 'warning' | 'info' | 'light' | 'dark' | 'link' | 'success'
 
 export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
     type?: ButtonType
